af9edb74185d6e9588d70137bd029949f4d3bd02
Fabian Muscat <fabian.muscat.d102385@mcast.edu.mt>
2020-11-18 17:57:31 +0100

    Notes
    5. Conditial Statements
    6. Repetition Control Statements

---
+++ Intro to OOP/Conditional Statements/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace Conditional_Statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // if else, if, nested if else, switch, break, continue
+
+            // == => compares the values of two or more variables
+            // != => checks if two or more variables are NOT equal
+            // > => greater than
+            // < => less than
+            // >= greater than or equal to
+            // <= less than or equal to
+
+            Console.Write("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (age > 18)
+                Console.WriteLine("You can drink");
+            else
+                Console.WriteLine("You cannot drink");
+
+            Console.Write("Enter mark: ");
+            int mark = Convert.ToInt32(Console.ReadLine());
+            char grade = Char.MinValue;
+
+            // && => shorthand AND operator
+            // || => shorthand OR operator
+            // ! => NOT operator
+            if ((mark >= 0) && (mark <= 100))
+            {
+                if (mark >= 90)
+                    grade = 'A';
+                else if (mark >= 80)
+                    grade = 'B';
+                else if (mark >= 70)
+                    grade = 'C';
+                else if (mark >= 50)
+                    grade = 'D';
+                else 
+                    grade = 'F';
+            }
+
+            Console.Write("Enter month number: ");
+            int monthNum = Convert.ToInt32(Console.ReadLine());
+
+
+            /* The switch statement is the same as
+             * a nested if statement except case branching is used
+             * instead of "if else". Each case contains an exact
+             * possible value that can be placed inside the variable
+             * which the switch is being performed on. A case is like
+             * a condition.
+             *
+             * In a switch, logical and mathematical operators cannot be used but only
+             * exact values.
+             * The syntax for the switch statement is as follows:
+             *
+             * switch (<variable>) {
+             *    case <condition>:
+             *      <statement>;
+             *      break;
+             *
+             *    default:
+             *      <statement>
+             *      break;
+             *
+             * default => this branch is only accessed if the value being compared
+             * does not exist in any of the cases.
+             */
+            switch (monthNum)
+            {
+                case 1:
+                    Console.WriteLine("January");
+                    break;
+
+                case 2:
+                    Console.WriteLine("February");
+                    break;
+
+                case 3:
+                    Console.WriteLine("March");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid Month");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Intro to OOP/Repetition Control Statements/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+
+namespace Repetition_Control_Statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ++ => increment (adds one to the current value)
+            // -- => decrement (subtracts one from the current value)
+
+            // for loop
+            /* This is a predetermined loop which means that the number of times
+             * that the code inside the loop is going to be executed is known prior
+             * to the execution of the code.
+             * The syntax for a for loop is as follows:
+             *
+             * for (int i = 0; i < x; i++) {
+             *      <statement>
+             * }
+             *
+             * int i = 0; => initialisation
+             * i < x; => loop condition where x is the number of times the loop will be executed
+             * i++ => iterator (i will be incremented by 1 until the loop condition becomes true)
+             */
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine(i);
+            }
+            /* Output:
+             * 0
+             * 1
+             * 2
+             * 3
+             */
+
+            // while loop
+            /* The while loop is not a predetermined loop which means that the amount of
+             * times the loop will be executed is unknown. The loop will break only if the
+             * condition becomes true. This loop will be executed 0 or more times depending on the
+             * loop condition.
+             * The syntax for a while loop is as follows:
+             *
+             * while (<condition>) {
+             *      <statement>
+             *      <statement>
+             * }
+             */
+            int counter = 10;
+            while (counter > 0)
+            {
+                Console.WriteLine("The counter is: " + counter);
+                counter--;
+            }
+            /* Output:
+             * The counter is: 10
+             * The counter is: 9
+             * The counter is: 8
+             * The counter is: 7
+             * The counter is: 6
+             * The counter is: 5
+             * The counter is: 4
+             * The counter is: 3
+             * The counter is: 2
+             * The counter is: 1
+             */
+
+            Console.WriteLine();
+
+            //do... while loop
+            /* The do while loop is the same as the while loop except that this loop
+             * must be executed at least once.
+             * The syntax for the do while loop is as follows:
+             *
+             * do {
+             *    <statement>
+             *    <statement>
+             * } while (<condition>);
+             */
+
+            counter = 10;
+            do
+            {
+                Console.WriteLine("The counter is: " + counter);
+                counter--;
+            } while (counter > 0);
+            /* Output:
+             * The counter is: 10
+             * The counter is: 9
+             * The counter is: 8
+             * The counter is: 7
+             * The counter is: 6
+             * The counter is: 5
+             * The counter is: 4
+             * The counter is: 3
+             * The counter is: 2
+             * The counter is: 1
+             */
+
+            // break statement
+            /* The break statement is used to terminate or break a loop.
+             * This is normally inside of a condition. If a condition is met,
+             * the loop is terminated.
+             */
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+                if (i == 5)
+                    break;
+            }
+            /* Output:
+             * 0
+             * 1
+             * 2
+             * 3
+             * 4
+             * 5
+             */
+
+            Console.WriteLine();
+
+            // continue statement
+            /* The continue statement breaks or skips a specific iteration in
+             * a loop if a given condition is met, and continues with the next iteration.
+             */
+            for (int i = 0; i < 20; i++)
+            {
+                if (i > 5 && i < 15)
+                    continue;
+                Console.WriteLine(i);
+            }
+            /* Output:
+             * 0
+             * 1
+             * 2
+             * 3
+             * 4
+             * 5
+             * 15
+             * 16
+             * 17
+             * 18
+             * 19
+             */
+
+            Console.ReadKey();
+        }
+    }
+}
